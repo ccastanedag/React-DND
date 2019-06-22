4ffdb9379b23c2510b4ff8d235818ea03a3588ee
carlos <carlos.castaneda.gutierrez@gmail.com>
2019-06-22 15:50:13 -0500

    Createe and style list of data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import initialData from './initial-data'
+import Column from './column'
+import '@atlaskit/css-reset'
+
+class App extends Component {
+  state = initialData
+
+  render() {
+    return  this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+
+      return <Column key={column.id} column={column} tasks={tasks}/>
+    })
+  }
+}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/task.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  border: 1px solid lightgrey
+  border-radius: 2px
+  padding: 8px
+  margin-bottom: 8px`
+
+export default class task extends Component {
+  render() {
+    return <Container>{this.props.task.content}</Container>
+  }
+}
