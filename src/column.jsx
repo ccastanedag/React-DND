27bec8f62208b2eba067b0bf8e247aa3726c183c
carlos <carlos.castaneda.gutierrez@gmail.com>
2019-06-22 19:34:39 -0500

    Implement Drag and Drop behaviour

--- src/column.jsx
+++ src/column.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import Task from './task'
+import { Droppable } from 'react-beautiful-dnd'
 
 const Container = styled.div`
   margin: 8px
@@ -17,9 +18,17 @@ export class column extends Component {
     return (
       <Container>
         <Title>{this.props.column.title}</Title>
-        <TaskList>
-          {this.props.tasks.map(task => <Task key={task.id} task={task} />)}
-        </TaskList>
+        <Droppable droppableId={this.props.column.id}>
+          {provided => (
+            <TaskList
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {this.props.tasks.map((task,index) => <Task key={task.id} task={task} index={index} />)}
+              {provided.placeholder}
+            </TaskList>
+          )}
+        </Droppable>
       </Container>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import initialData from './initial-data'
 import Column from './column'
 import '@atlaskit/css-reset'
+import { DragDropContext } from 'react-beautiful-dnd'
 
 class App extends Component {
   state = initialData
 
+  onDragEnd = (result) => {
+
+  }
+
   render() {
-    return  this.state.columnOrder.map(columnId => {
+    return <DragDropContext onDragEnd={this.onDragEnd}>
+    {this.state.columnOrder.map(columnId => {
       const column = this.state.columns[columnId];
       const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
 
-      return <Column key={column.id} column={column} tasks={tasks}/>
-    })
+      return <Column key={column.id} column={column} tasks={tasks} />
+    })}
+    </DragDropContext>
   }
 }
 
