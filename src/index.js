096e915ecc1c73a901e2b835082e593f692f1489
carlos <carlos.castaneda.gutierrez@gmail.com>
2019-06-23 00:23:53 -0500

    Persist list re-ordering

--- src/index.js
+++ src/index.js
@@ -8,18 +8,44 @@ import { DragDropContext } from 'react-beautiful-dnd'
 class App extends Component {
   state = initialData
 
-  onDragEnd = (result) => {
+  onDragEnd = result => {
+    const { destination, source, draggableId } = result;
+
+    if (!destination) { return }
+
+    if (destination.droppableId === source.droppableId &&
+      destination.index === source.index) { return }
+
+    const column = this.state.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    }
+
+    this.setState(newState)
 
   }
 
   render() {
     return <DragDropContext onDragEnd={this.onDragEnd}>
-    {this.state.columnOrder.map(columnId => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+      {this.state.columnOrder.map(columnId => {
+        const column = this.state.columns[columnId];
+        const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
 
-      return <Column key={column.id} column={column} tasks={tasks} />
-    })}
+        return <Column key={column.id} column={column} tasks={tasks} />
+      })}
     </DragDropContext>
   }
 }
